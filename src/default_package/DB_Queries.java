df52009b543931b4943dd34e8b3c123ec7e941e6
Deniz Yunus Göğüş <58386750+DenizYunus@users.noreply.github.com>
2021-12-21 02:47:57 +0300

    Initial_Commit

---
+++ src/default_package/DB_Queries.java
@@ -0,0 +1,152 @@
+package default_package;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+
+import java.awt.image.*;
+import java.awt.*;
+
+public class DB_Queries {
+
+	String serverUrl;
+	String username;
+
+	static Connection dbCon;
+
+	public DB_Queries(String username, String serverUrl) {
+		super();
+		this.username = username;
+		try {
+			dbCon = DriverManager.getConnection(
+					(serverUrl != null ? serverUrl : "jdbc:mysql://localhost:3306/java_project"), "root", "");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet GetFeed()
+	{
+		ResultSet rs;
+
+		try {
+			Statement stmt = dbCon.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery("SELECT * FROM `posts` ORDER BY upload_date DESC LIMIT 20");
+
+			return rs;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public void ExecuteQuery(String _query) {
+		try {
+			Statement stmt = dbCon.createStatement();
+			stmt.execute(_query);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet GetQuery(String _query) {
+		ResultSet rs;
+
+		try {
+			Statement stmt = dbCon.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			rs = stmt.executeQuery(_query);
+
+			return rs;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public ImageIcon GetUserProfilePic(String _username) {
+		Integer photoSizes = 110;
+		String query = "SELECT profile_pic FROM users where username = \"" + _username + "\"";
+		ResultSet queryResult = GetQuery(query);
+		System.out.println(query);
+
+		try {
+			queryResult.next();
+			String resultLink = queryResult.getString("profile_pic");
+			BufferedImage masterNotResized = ImageIO.read(new URL(resultLink).openStream());
+			BufferedImage master = resizeImage(masterNotResized, photoSizes, photoSizes);
+
+			int diameter = Math.min(photoSizes, photoSizes); // master.getWidth(), master.getHeight());
+			BufferedImage mask = new BufferedImage(photoSizes, photoSizes,
+					/* master.getWidth(), master.getHeight(), */BufferedImage.TYPE_INT_ARGB);
+
+			Graphics2D g2d = mask.createGraphics();
+			applyQualityRenderingHints(g2d);
+			g2d.fillOval(0, 0, diameter - 1, diameter - 1);
+			g2d.dispose();
+
+			BufferedImage masked = new BufferedImage(diameter, diameter, BufferedImage.TYPE_INT_ARGB);
+			g2d = masked.createGraphics();
+			applyQualityRenderingHints(g2d);
+			int x = (diameter - master.getWidth()) / 2;
+			int y = (diameter - master.getHeight()) / 2;
+			g2d.drawImage(master, x, y, null);
+			g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
+			g2d.drawImage(mask, 0, 0, null);
+			g2d.dispose();
+
+			return new ImageIcon(masked);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	public static void applyQualityRenderingHints(Graphics2D g2d) {
+
+		g2d.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_ENABLE);
+		g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+		g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+		g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+	}
+
+	BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) {
+		BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+		Graphics2D graphics2D = resizedImage.createGraphics();
+		graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+		graphics2D.dispose();
+		return resizedImage;
+	}
+
+	public String CreateRandomID() {
+		Random rand = new Random();
+		int upperBound = 999999999; // 9 digit ID
+		int lowerBound = 100000000;
+
+		while (true) {
+			Integer idGenerated = lowerBound + rand.nextInt(upperBound - lowerBound);
+
+			try {
+				ResultSet set = GetQuery("SELECT 1 FROM posts WHERE id = \"" + idGenerated + "\";");
+				if (!set.isBeforeFirst()) {
+					return idGenerated.toString();
+				} else continue;
+			} catch (Exception e) {
+				e.printStackTrace();
+				return null;
+			}
+		}
+	}
+}
---
+++ src/default_package/Main_Panel.java
@@ -0,0 +1,294 @@
+package default_package;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JFileChooser;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Main_Panel extends JFrame {
+
+	private JPanel contentPane;
+	private JSplitPane splitPane;
+	private JLabel welcomeUserLabel;
+	JLabel profilePicLabel;
+	JLabel uploadResultLabel;
+	JTextArea captionTextArea;
+	
+	//INHERITED USER VARIABLES
+	public String username = "opsf";
+	
+	
+	DB_Queries dbQueries;
+	File imageToShare;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Main_Panel frame = new Main_Panel();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main_Panel() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 674, 552);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(200, 0));
+		setContentPane(contentPane);
+		
+		splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.7);
+		splitPane.setDividerSize(0);
+		contentPane.add(splitPane, BorderLayout.CENTER);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		splitPane.setLeftComponent(scrollPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.GRAY);
+		splitPane.setRightComponent(panel);
+		panel.setLayout(null);
+		
+		welcomeUserLabel = new JLabel("Welcome back, {username}");
+		welcomeUserLabel.setVerticalAlignment(SwingConstants.TOP);
+		welcomeUserLabel.setFont(new Font("Rockwell", Font.PLAIN, 17));
+		welcomeUserLabel.setBounds(10, 10, 175, 45);
+		panel.add(welcomeUserLabel);
+		
+		JButton uploadButton = new JButton("UploadPost");
+		uploadButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					if (imageToShare == null)
+					{
+						uploadResultLabel.setForeground(Color.RED);
+						uploadResultLabel.setText("Don't forget to pick your photo!");
+						return;
+					}
+					String postId = dbQueries.CreateRandomID();
+					String resultFilePath = uploadFile(postId, imageToShare.getAbsolutePath());
+					
+					//INSERT INTO POSTS DATABASE
+					String postsQuery = "INSERT INTO `posts` (`id`, `upload_date`, `file_path`, `uploader`, `caption`) VALUES"
+							+ "('" + postId + "', CURRENT_TIME(), 'localhost/java_project/" + resultFilePath + "', '" + username + "', '" + captionTextArea.getText() + "');";
+					dbQueries.ExecuteQuery(postsQuery);
+					//System.out.println(postsQuery);
+					
+					//INSERT INTO USER POST COUNT AND POSTS
+					String usersQuery = "SELECT post_count FROM `users` WHERE username = \"" + username + "\"";
+					//System.out.println(usersQuery);
+					ResultSet queryResult = dbQueries.GetQuery(usersQuery); //Get post count
+					queryResult.beforeFirst();
+					queryResult.next();
+					System.out.println(queryResult.getInt(1));
+					Integer newPostCount = queryResult.getInt(1) + 1;
+					
+					String userPostsQuery = "SELECT posts FROM `users` WHERE username = \"" + username + "\""; 
+					queryResult = dbQueries.GetQuery(userPostsQuery); //Get post ids
+					queryResult.beforeFirst();
+					queryResult.next();
+					String postIds = queryResult.getString(1) + postId + ", ";
+					
+					//HERE UPDATE THE VALUES
+					String usersUpdateQuery = "UPDATE users SET post_count = " + newPostCount.toString() + " WHERE username = \"" + username + "\"";
+					//System.out.println(usersUpdateQuery);
+					dbQueries.ExecuteQuery(usersUpdateQuery);
+					usersUpdateQuery = "UPDATE users SET posts = \"" + postIds + "\" WHERE username = \"" + username + "\"";
+					System.out.println(usersUpdateQuery);
+					dbQueries.ExecuteQuery(usersUpdateQuery);
+					
+					imageToShare = null;
+					
+					uploadResultLabel.setForeground(Color.GREEN);
+					uploadResultLabel.setText("<html>&ensp;You've successfully<br>uploaded your photo :)</html>");
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				} catch (NumberFormatException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		uploadButton.setBounds(0, 315, 195, 40);
+		panel.add(uploadButton);
+
+		captionTextArea = new JTextArea();
+		captionTextArea.setBounds(0, 221, 200, 88);
+		panel.add(captionTextArea);
+		
+		profilePicLabel = new JLabel("ProfilePicture");
+		profilePicLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		profilePicLabel.setBounds(33, 55, 123, 117);
+		panel.add(profilePicLabel);
+		
+		JButton selectPictureButton = new JButton("Select Picture");
+		selectPictureButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+				int result = fileChooser.showOpenDialog(panel);
+				if (result == JFileChooser.APPROVE_OPTION) {
+				    imageToShare = fileChooser.getSelectedFile();
+				}
+			}
+		});
+		selectPictureButton.setBounds(0, 198, 195, 21);
+		panel.add(selectPictureButton);
+		
+		uploadResultLabel = new JLabel("New label");
+		uploadResultLabel.setVerticalAlignment(SwingConstants.TOP);
+		uploadResultLabel.setForeground(Color.GRAY);
+		uploadResultLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		uploadResultLabel.setBounds(0, 355, 195, 45);
+		panel.add(uploadResultLabel);
+        
+		
+		//START DATABASE PROCESSES
+		
+		dbQueries = new DB_Queries(username, null);
+		ImageIcon a = dbQueries.GetUserProfilePic("opsf");
+		profilePicLabel.setIcon(a);
+		welcomeUserLabel.setText("<html>Welcome back,<br><font color=\"#0000FF\" size=4>" + username + "</font></html>");
+		
+	}
+	
+    public static String uploadFile(String fileId, String filePath/*, String serverURL*/) throws IOException {
+    	URLConnection conn = null;
+        OutputStream os = null;
+        InputStream is = null;
+
+        String resultFilePath = "";
+        try {
+        	String CrLf = "\r\n";
+            URL url = new URL("http://localhost/java_project/UploadFile.php");
+            System.out.println("url:" + url);
+            conn = url.openConnection();
+            conn.setDoOutput(true);
+
+            //String postData = "";
+
+            //System.out.println(filePath);
+            File initialFile = new File(filePath);
+            InputStream imgIs = new FileInputStream(initialFile);
+            byte[] imgData = new byte[imgIs.available()];
+            imgIs.read(imgData);
+
+            String message1 = "";
+            message1 += "-----------------------------4664151417711" + CrLf;
+            message1 += "Content-Disposition: form-data; name=\"uploadedfile\"; filename=\"" + fileId + ".jpg\""
+                    + CrLf;
+            message1 += "Content-Type: image/jpeg" + CrLf;
+            message1 += CrLf;
+
+            // the image is sent between the messages in the multipart message.
+
+            String message2 = "";
+            message2 += CrLf + "-----------------------------4664151417711--"
+                    + CrLf;
+
+            conn.setRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=---------------------------4664151417711");
+            // might not need to specify the content-length when sending chunked
+            // data.
+            conn.setRequestProperty("Content-Length", String.valueOf((message1
+                    .length() + message2.length() + imgData.length)));
+
+            System.out.println("open os");
+            os = conn.getOutputStream();
+
+            System.out.println(message1);
+            os.write(message1.getBytes());
+
+            // SEND THE IMAGE
+            int index = 0;
+            int size = 1024;
+            do {
+                //System.out.println("write:" + index);
+                if ((index + size) > imgData.length) {
+                    size = imgData.length - index;
+                }
+                os.write(imgData, index, size);
+                index += size;
+            } while (index < imgData.length);
+            System.out.println("written:" + index);
+
+            System.out.println(message2);
+            os.write(message2.getBytes());
+            os.flush();
+
+            System.out.println("open is");
+            is = conn.getInputStream();
+
+            char buff = 512;
+            int len;
+            byte[] data = new byte[buff];
+            do {
+                System.out.println("READ");
+                len = is.read(data);
+
+                if (len > 0) {
+                    System.out.println(new String(data, 0, len));
+                    if (!(new String(data, 0, len).length() < 2)) resultFilePath = new String(data, 0, len);
+                }
+            } while (len > 0);
+
+            System.out.println("DONE");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Close connection");
+            try {
+                os.close();
+            } catch (Exception e) {
+            }
+            try {
+                is.close();
+            } catch (Exception e) {
+            }
+        }
+        return resultFilePath;
+    }
+}
